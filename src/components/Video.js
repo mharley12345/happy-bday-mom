1efc8d6699dcee4cac14d8df0c020d2c4f3e9af4
Mike Harley <mharley12345@gmail.com>
2020-01-28 00:02:32 -0800

    .

--- src/components/Video.js
+++ src/components/Video.js
@@ -6,7 +6,7 @@ const Video = (props) => {
     return (
         <ReactPlayer
     
-         height="630px" width="930px" playing="true" controls='true' loop='true' 
+         height="630px" width="930px" playing={true} controls={true} loop={true} 
          url={mom} type="video/mp4"
           style={{marginTop:"70px",border:"5px solid black",boxShadow:"0 0 40px 40px black"}}
          />
