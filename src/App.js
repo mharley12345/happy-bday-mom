2556e85d351a67040cc0f1d5839bac656ffb01bf
Mike Harley <mharley12345@gmail.com>
2020-01-27 17:24:53 -0800

    Initial commit from Create React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Wrapper , Card} from 'bushido-strap'
+import './App.scss';
+
+function App() {
+  return (
+     <Wrapper>
+       <Card>
+         <h2>Hello World!!</h2>
+       </Card>
+     </Wrapper>
+  );
+}
+
+export default App;
